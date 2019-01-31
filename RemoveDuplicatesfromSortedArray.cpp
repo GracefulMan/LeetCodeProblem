db3e204225e130210c613a13c04e1e0157df6eef
GracefulMan <41041321+GracefulMan@users.noreply.github.com>
2019-01-31 19:19:32 +0800

    easy

--- RemoveDuplicatesfromSortedArray.cpp
+++ RemoveDuplicatesfromSortedArray.cpp
@@ -4,24 +4,16 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-int removeDuplicates(vector<int>& nums) {
-    if(nums.size()<=1){
+int removeElement(vector<int>& nums, int val) {
+    for (vector<int>::iterator it = nums.begin(); it != nums.end();) {
+        if (*it == val) nums.erase(it);
+        else it++;
         return nums.size();
     }
-   int i=0,j=0;
-    while(j<nums.size()){
-        if(nums[i]!=nums[j]){
-            i++;
-            nums[i]=nums[j];
-        }
-        j++;
-    }
-    return i+1;
-
 }
 int main(){
     vector<int> nums={1,1,2,2,3,4,4,5,6};
-    int length = removeDuplicates(nums);
+    int length = removeElement(nums,6);
     for(int i=0;i<length;i++){
         cout<<nums[i]<<'\t';
     }
