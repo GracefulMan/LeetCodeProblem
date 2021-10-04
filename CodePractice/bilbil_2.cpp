3481652b1e75d3e6e6dd619ce7368aa9cdbec58f
HongyingMa <1729037772@sjtu.edu.cn>
2021-10-04 15:42:26 +0800

    variant of merge sort

--- CodePractice/bilbil_2.cpp
+++ CodePractice/bilbil_2.cpp
@@ -40,13 +40,102 @@ void mergeSort(int *arr, int left, int right){
     }
 
 }
+// 小和问题，详细看笔记
+//tested
+int mergeSmallSum(int *arr, int left, int mid, int right){
+    int *tmp = new int [right - left + 1];
+    int p0=left, p1=mid+1, i=0;
+    int res = 0;
+    while (p0 <= mid && p1 <= right){
+        res += arr[p0] < arr[p1] ? arr[p0] * (right - p1 + 1) : 0;
+        tmp[i++] = arr[p0] < arr[p1] ? arr[p0++] : arr[p1++];
+    }
+    while (p0 <= mid) tmp[i++] = arr[p0++];
+    while (p1 <= right) tmp[i++] = arr[p1++];
+
+    for(int j=0; j < right - left + 1; j++)
+        arr[left + j] = tmp[j];
+    delete [] tmp;
+    return res;
+}
+
+int getSmallSum(int *arr, int left, int right){
+    if(left == right) return 0;
+    else{
+        int mid = left +((right - left) >> 1);
+        int leftSmallSum = getSmallSum(arr, left, mid);
+        int rightSmallSum = getSmallSum(arr, mid + 1, right);
+        int mergeSum = mergeSmallSum(arr, left, mid, right);
+        return leftSmallSum + rightSmallSum + mergeSum;
+    }
+}
+
+int BruteSearchForSmallSum(int *arr, int n){
+    int res = 0;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<i;j++) {
+            res += arr[j] < arr[i] ? arr[j] : 0;
+        }
+    }
+    return res;
+}
+
+void printArray(int *arr, int n){
+    for(int i=0;i<n;i++)
+        cout<<arr[i]<<'\t';
+    cout<<endl;
+}
+
+//逆序对问题
+void BruteSearchForInversePair(int *arr, int n){
+    for(int i=0;i<n; i++)
+        for(int j=i+1;j<n;j++){
+            if(arr[i] > arr[j]){
+                cout<<"{"<<arr[i]<<","<<arr[j]<<"}\t";
+            }
+        }
+}
+void mergeInversePair(int *arr, int left, int mid, int right){
+    int *tmp = new int [right - left + 1];
+    int p0=left, p1=mid+1, i=0;
+    while (p0 <= mid && p1 <= right){
+        if(arr[p0] > arr[p1]){
+            int index = p1;
+            while (index <= right){
+                cout <<"{"<<arr[p0]<<","<<arr[index]<<"}\t";
+                index++;
+            }
+        }
+        tmp[i++] = arr[p0] > arr[p1] ? arr[p0++]: arr[p1++];
+    }
+    while (p0 <= mid) tmp[i++] = arr[p0++];
+    while (p1 <= right) tmp[i++] = arr[p1++];
+    for(int j=0;j< right - left + 1; j++){
+        arr[left + j] = tmp[j];
+    }
+    delete [] tmp;
+}
+
+void InversePair(int *arr, int left ,int right){
+    if (left == right) return;
+    else{
+        int mid = left + ((right - left)>>2);
+        InversePair(arr, left, mid);
+        InversePair(arr, mid + 1, right);
+        mergeInversePair(arr, left, mid, right);
+    }
+}
+
 
 int main(){
-    int arr[] = {1,3,4,322,3,1,54, 435};
-    int res = getMaxValue(arr, 0, 8);
-    mergeSort(arr, 0, 7);
-    for(int i=0;i<8;i++){
-        cout<<arr[i]<<'\t';
-    }
-    cout<<endl<< res<<endl;
+    int arr[] = {1,3,4,2, 5};
+    int n = 5;
+    BruteSearchForInversePair(arr, n);
+    cout<<endl;
+    InversePair(arr, 0, n-1);
+//    cout<<"Small Sum by BreteSearch:"<<BruteSearchForSmallSum(arr, n)<<endl;
+//    cout<<"Small Sum:"<<getSmallSum(arr, 0, n-1)<<endl;
+    printArray(arr, n);
+
+
 }
--- CodePractice/review.cpp
+++ CodePractice/review.cpp
@@ -21,6 +21,7 @@ void swap(int *arr, int i, int j){
 
 //2. find the digit which appear only once while all others appear twice.
 
+// tested!
 int onlyOnceDigits(int *arr, int n){
     int res = 0;
     for(int i=0;i<n;i++)
@@ -29,6 +30,7 @@ int onlyOnceDigits(int *arr, int n){
 }
 //3. find two digits which appear only once while all others appear twice.
 
+// tested!
 int onlyOnceTwoDidits(int *arr, int n){
     int res = 0;
     for(int i=0;i<n;i++)
@@ -36,16 +38,105 @@ int onlyOnceTwoDidits(int *arr, int n){
     int rightFirstOne = (~res + 1) & res;
     int onceDigit = 0;
     for(int i=0;i<n;i++){
-        if ((rightFirstOne & arr[i]) != 0){
+        if ((rightFirstOne & arr[i]) == 0){
             onceDigit ^= arr[i];
         }
     }
-    int anotherRes = res ^ onceDigit;
+    int anotherRes = (res ^ onceDigit);
     cout<<"two digits:"<<anotherRes<<'\t'<<onceDigit<<endl;
 }
 
 // sort algorithms.
 
+//tested
+void selectSort(int *arr, int n){
+    for(int i=0;i<n;i++)
+        for(int j=i+1; j<n;j++)
+            if(arr[j]<arr[i])
+                swap(arr, i, j);
+}
+
+// tested;
+void bubbleSort(int  *arr, int n){
+    for(int i=n-1;i>0;i--){
+        bool flag = true;
+        for(int j=0;j<i;j++){
+            if(arr[j]>arr[j+1]) {
+                swap(arr, j, j + 1);
+                flag = false;
+            }
+        }
+        if (flag) break;
+    }
+}
+
+// recursive application
+/*
+ * complexity theorem:
+ * Master equation: T(N) = a * T(N/b) + O(N^d)
+ * if log_b a < d, complexity=O(N^d)
+ * if log_b a > d, complexity=O(N^log_b a)
+ * if log_b a =d, complexity= O(N^d log N)
+ */
+
+//tested;
+int getMaxValue(int *arr, int left, int right){
+    // a = 2, b=2, d=0, log _b a=1>0, complexity: O(N)
+    if(left == right) return arr[left];
+    else{
+        int mid = left + ((right - left)>>1);
+        int leftMaxValue = getMaxValue(arr, left, mid);
+        int rightMaxValue = getMaxValue(arr, mid + 1, right);
+        return leftMaxValue > rightMaxValue?leftMaxValue : rightMaxValue;
+    }
+}
+
+void merge(int *arr, int left, int mid, int right){
+    int *res = new int [right - left + 1];
+    int p0=left, p1=mid+1, i=0;
+    while (p0 <= mid && p1 <= right){
+        res[i++] = arr[p0] < arr[p1] ? arr[p0++] : arr[p1++];
+    }
+    while (p0 <= mid)
+        res[i++] = arr[p0++];
+    while (p1 <=right)
+        res[i++] = arr[p1++];
+    for(int j=0;j<right - left + 1; j++){
+        arr[j + left] = res[j];
+    }
+    delete [] res;
+}
+
+//tested;
+void mergeSort(int *arr, int left, int right){
+    /*
+     * a = 2, b=2, d=1; log_b a=1 == d,
+     * complexity = O(N^d log N)=O(N log N)
+     * */
+
+    if(left == right) return;
+    else{
+        int mid = left + ((right - left)>>1);
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
+}
+
+void printArray(int *arr, int n){
+    for(int i=0;i<n;i++)
+        cout<<arr[i]<<'\t';
+    cout<<endl;
+}
+
+
+int main(){
+    int arr[]={1, 3, 5, 7, 9, 2, 4, 6, 8, 10, 10, 8, 6, 4, 2, 9, 7, 5, 3, 1, 42, 24};
+    int n=22;
+    cout<<getMaxValue(arr, 0, n-1);
+
+
+}
 
 
 
